063c0c6094ac502dbdbe585d9f5923cee45f73df
Adrevolution <publisher@adrevolution.io>
2025-07-13 19:42:06 +0530

    Add files via upload

---
+++ proposal/script.js
@@ -0,0 +1,13 @@
+function nextPage(page) {
+  window.location.href = page;
+}
+
+function showLove() {
+  document.body.innerHTML = `
+    <div class="centered">
+      <h1 style="font-size:3rem;">Yesss! 😭💍</h1>
+      <p style="font-size:1.5rem;">You just made me the happiest person alive 💞</p>
+      <p style="font-size:1.2rem;">Let’s build a lifetime together 💑</p>
+    </div>
+  `;
+}
